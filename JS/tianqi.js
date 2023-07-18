dccfaef5df8cae3c9826a5cf144f5c605c38e0ec
Gin8527 <119291050+Gin8527@users.noreply.github.com>
2023-07-18 15:41:03 +0800

    Update tianqi.js

--- JS/tianqi.js
+++ JS/tianqi.js
@@ -1,4 +1,4 @@
-const params = getParams(params);
+const params = getParams($argument);
 const cityId = params.cityId || "101210411";
 const apiUrl = `http://t.weather.sojson.com/api/weather/city/${cityId}`;
 
