b54a24da05cd144a12ae2f4495a4090a49b3a029
Gin8527 <119291050+Gin8527@users.noreply.github.com>
2023-07-19 23:55:26 +0800

    Update appfree.js

--- JS/appfree.js
+++ JS/appfree.js
@@ -1,18 +1,17 @@
-const now = new Date();
-const hour = now.getHours();
-const minute = now.getMinutes();
+const JOKE_API_URL = 'https://official-joke-api.appspot.com/random_joke';
 
-if (hour === 9 && minute === 0) {
-  $notification.post('上班提醒', '', '现在是上班时间，请准备工作！');
-} else if (hour === 18 && minute === 0) {
-  $notification.post('下班提醒', '', '现在是下班时间，辛苦一天，可以放松一下了！');
-} else if (hour < 18) {
-  const remainingHours = 18 - hour;
-  const remainingMinutes = (remainingHours - 1) * 60 + (60 - minute);
-  $notification.post('下班倒计时', '', `距离下班还有 ${remainingHours} 小时 ${remainingMinutes} 分钟`);
-} else {
-  const nextDay = new Date(now.getTime() + 24 * 60 * 60 * 1000);
-  const remainingHours = 24 - (hour - 18);
-  const remainingMinutes = (remainingHours - 1) * 60 + (60 - minute);
-  $notification.post('下班倒计时', '', `距离下班还有 ${remainingHours} 小时 ${remainingMinutes} 分钟`);
-}
+$httpClient.get(JOKE_API_URL, (error, response, body) => {
+  if (error) {
+    console.error('获取笑话失败:', error);
+    $done();
+    return;
+  }
+
+  const jokeData = JSON.parse(body);
+  const setup = jokeData.setup;
+  const punchline = jokeData.punchline;
+  const joke = `${setup}\n\n${punchline}`;
+
+  $notification.post('笑话', '', joke);
+  $done({ result: joke });
+});
