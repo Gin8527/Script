08fa5e25afa25d5222adbb1daf9aa5f4f1910892
Gin8527 <119291050+Gin8527@users.noreply.github.com>
2023-07-19 22:42:33 +0800

    Update appfree.js

--- JS/appfree.js
+++ JS/appfree.js
@@ -1,4 +1,4 @@
-const url = "https://gofans.cn/limited/ios";
+const url= "https://gofans.cn/limited/ios";
 
 const headers = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
