f63641023d5d876216b25d03e21ec682677a45d5
Gin8527 <119291050+Gin8527@users.noreply.github.com>
2023-07-19 23:10:30 +0800

    Update appfree.js

--- JS/appfree.js
+++ JS/appfree.js
@@ -1,19 +1,6 @@
 const CAIYUN_API_KEY = 'fmqppLgQRcTNFjpB'; // 替换为你的彩云天气 API 密钥
-const TENCENT_MAP_API_KEY = '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'; // 替换为你的腾讯地图 API 密钥
+const TENCENT_MAP_API_KEY = 'y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'; // 替换为你的腾讯地图 API 密钥
 const CITY_CODE = '330200'; // 替换为你的城市代码
-
-// 浙江省城市代码
-// 杭州市：330100
-// 宁波市：330200
-// 温州市：330300
-// 嘉兴市：330400
-// 湖州市：330500
-// 绍兴市：330600
-// 金华市：330700
-// 衢州市：330800
-// 舟山市：330900
-// 台州市：331000
-// 丽水市：331100
 
 const CAIYUN_API_URL = `https://api.caiyunapp.com/v2.5/${CAIYUN_API_KEY}/${TENCENT_MAP_API_KEY}/${CITY_CODE}/weather.json`;
 const TENCENT_MAP_API_URL = `https://apis.map.qq.com/ws/geocoder/v1/?key=${TENCENT_MAP_API_KEY}&location=${CITY_CODE}`;
@@ -51,7 +38,7 @@ $httpClient.get(CAIYUN_API_URL, (error, response, body) => {
       weatherForecast += `\n${hour}小时后: ${forecast.value}`;
     });
 
-    console.log(weatherForecast);
+    $notification.post('实时天气情况', '', weatherForecast);
     $done();
   });
 });
