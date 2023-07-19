fcb63d36a6d84333793ad2b78e70d3ec2c23595c
Gin8527 <119291050+Gin8527@users.noreply.github.com>
2023-07-19 22:50:25 +0800

    Update appfree.js

--- JS/appfree.js
+++ JS/appfree.js
@@ -1,39 +1,36 @@
-const url= "https://gofans.cn/limited/ios";
-
+const url = "https://gofans.cn/limited/ios";
 const headers = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
 };
+const LOG_URL = "http://localhost:3000"; // 替换为你本地日志服务器的地址
 
 if (typeof $task !== 'undefined') {
-
   $task.fetch({ url: url, headers: headers }).then(response => {
     handleResponse(response.body);
   }, reason => {
-    console.log('获取应用信息失败:', reason.error);
+    sendLog('获取应用信息失败: ' + reason.error);
     $done();
   });
 } else if (typeof $httpClient !== 'undefined' && typeof $notification !== 'undefined') {
-
   $httpClient.get({ url: url, headers: headers }, function (error, response, body) {
     if (error) {
-      console.log('获取应用信息失败:', error);
+      sendLog('获取应用信息失败: ' + error);
       $done();
       return;
     }
     handleResponse(body);
   });
 } else if (typeof $request !== 'undefined' && typeof $notify !== 'undefined') {
-
   $httpClient.get({ url: url, headers: headers }, function (error, response, body) {
     if (error) {
-      console.log('获取应用信息失败:', error);
+      sendLog('获取应用信息失败: ' + error);
       $done();
       return;
     }
     handleResponse(body, $request.headers);
   });
 } else {
-  console.log('未知的脚本运行环境');
+  sendLog('未知的脚本运行环境');
   $done();
 }
 
@@ -54,10 +51,10 @@ function handleResponse(body, requestHeaders) {
   } else if (typeof $notification !== 'undefined') {
     $notification.post("AppStore限免APP", '', notificationContent);
   } else {
-    console.log('未知的通知函数');
+    sendLog('未知的通知函数');
   }
 
-  console.log(notificationContent); // 输出日志内容
+  sendLog(notificationContent); // 发送日志内容
 
   $done();
 }
@@ -88,4 +85,19 @@ function truncateDescription(description, maxLength) {
     return description.substring(0, maxLength) + '…';
   }
   return description;
+}
+
+function sendLog(message) {
+  const log = { message: message };
+  $httpClient.post({
+    url: LOG_URL,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(log)
+  }, function(error, response, body) {
+    if (error) {
+      console.log('发送日志失败:', error);
+    } else {
+      console.log('日志已发送:', message);
+    }
+  });
 }
