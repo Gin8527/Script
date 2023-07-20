86be8351271e0e16aa1f470e3da0efad26f37639
Gin8527 <119291050+Gin8527@users.noreply.github.com>
2023-07-20 15:32:13 +0800

    Update appstore-dailyfree.js

--- JS/appstore-dailyfree.js
+++ JS/appstore-dailyfree.js
@@ -1,89 +1,39 @@
-const url = "https://gofans.cn/limited/ios";
+const appName = "AppSliced";
 
-const headers = {
-  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
-};
-
-if (typeof $task !== 'undefined') {
-
-  $task.fetch({ url: url, headers: headers }).then(response => {
-    handleResponse(response.body);
-  }, reason => {
-    console.log('获取应用信息失败:', reason.error);
-    $done();
-  });
-} else if (typeof $httpClient !== 'undefined' && typeof $notification !== 'undefined') {
-
-  $httpClient.get({ url: url, headers: headers }, function (error, response, body) {
-    if (error) {
-      console.log('获取应用信息失败:', error);
-      $done();
-      return;
-    }
-    handleResponse(body);
-  });
-} else if (typeof $request !== 'undefined' && typeof $notify !== 'undefined') {
-
-  $httpClient.get({ url: url, headers: headers }, function (error, response, body) {
-    if (error) {
-      console.log('获取应用信息失败:', error);
-      $done();
-      return;
-    }
-    handleResponse(body, $request.headers);
-  });
-} else {
-  console.log('未知的脚本运行环境');
-  $done();
-}
-
-function handleResponse(body, requestHeaders) {
-  const appList = parseAppList(body);
-  const freeAppList = appList.filter(app => app.price === "Free");
-
-  let notificationContent = '';
-  const appCount = requestHeaders ? parseInt(requestHeaders['appCount']) || 8 : 8;
-  for (let i = 0; i < freeAppList.length && i < appCount; i++) {
-    const app = freeAppList[i];
-    const description = truncateDescription(app.description, 30);
-    notificationContent += `🆓${app.name}｜原价￥${app.originalPrice}\n`;
-  }
-
-  if (typeof $notify !== 'undefined') {
-
-    $notify("AppStore限免APP", '', notificationContent);
-  } else if (typeof $notification !== 'undefined') {
-
-    $notification.post("AppStore限免APP", '', notificationContent);
+$httpClient.get("https://appsliced.co/apps?sort=latest&threshold=all&price=free&l=nav", function(error, response, body) {
+  if (error) {
+    $.log(error);
+    $.msg(appName, "❌ Network Error", error);
+    $.done();
   } else {
-    console.log('未知的通知函数');
-  }
+    $.log("🚧 Getting AppSliced daily free apps...\n");
+    // $.log(body);
 
-  $done();
-}
-
-function parseAppList(html) {
-  const regex = /<div[^>]+class="column[^"]*"[^>]*>[\s\S]*?<strong[^>]+class="title[^"]*"[^>]*>(.*?)<\/strong>[\s\S]*?<b[^>]*>(.*?)<\/b>[\s\S]*?<div[^>]+class="price-original[^"]*"[^>]*>[^<]*<del[^>]*>(.*?)<\/del>[\s\S]*?<p[^>]+class="intro[^"]*"[^>]*>([\s\S]*?)<\/p>/g;
-  const appList = [];
-  let match;
-  while ((match = regex.exec(html)) !== null) {
-    const name = match[1];
-    const price = match[2];
-    const originalPrice = parseFloat(match[3]).toFixed(1);
-    const description = match[4].replace(/<.*?>/g, '').replace(/\n+/g, ' ').trim();
-    appList.push({
-      name: name,
-      price: price,
-      originalPrice: originalPrice,
-      description: description,
-    });
+    try {
+      const appsData = JSON.parse(body);
+      if (appsData && appsData.items && appsData.items.length > 0) {
+        const apps = appsData.items;
+        let notice = "";
+        for (const app of apps) {
+          const link = `https://apps.apple.com/us/app/id${app.id}\n`;
+          const name = `🟢${app.name}`;
+          notice += name + "\n" + link + "\n";
+        }
+        $.log(notice);
+        $.msg(appName, "Today's Free Apps are ready, click to view details👇", notice);
+        $.console.log(appName, "Today's Free Apps are ready, click to view details👇", notice);
+        $.done();
+      } else {
+        $.log("🔴 Failed to get app information!");
+        $.log(body);
+        $.msg(appName, "🔴 Failed to get app information!");
+        $.done();
+      }
+    } catch (e) {
+      $.log("🔴 Error parsing JSON response!");
+      $.log(body);
+      $.msg(appName, "🔴 Error parsing JSON response!");
+      $.done();
+    }
   }
-  return appList;
-}
-
-function truncateDescription(description, maxLength) {
-  if (description.length > maxLength) {
-    return description.substring(0, maxLength) + '…';
-  }
-  return description;
-}
+});
