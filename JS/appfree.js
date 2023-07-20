2872c2b536097737f3ee7c684339a4f58606870f
Gin8527 <119291050+Gin8527@users.noreply.github.com>
2023-07-20 13:27:19 +0800

    Update appfree.js

--- JS/appfree.js
+++ JS/appfree.js
@@ -7,14 +7,44 @@ const minute = now.getMinutes();
 const second = now.getSeconds();
 
 if (hour >= workStartTime && hour < workEndTime) {
-  const remainingSeconds = (workEndTime - hour - 1) * 3600 + (60 - minute - 1) * 60 + (60 - second);
-  $notification.post('下班倒计时', '', `距离下班还有 ${remainingSeconds} 秒`);
+  const remainingHours = workEndTime - hour - 1;
+  const remainingMinutes = 60 - minute - 1;
+  const remainingSeconds = 60 - second;
+  const currentTime = now.format("hh:mm:ss");
+
+  const message = `现在是北京时间 ${currentTime}\n距离下班还有 ${remainingHours} 小时 ${remainingMinutes} 分钟 ${remainingSeconds} 秒 😊`;
+  $notification.post('下班倒计时', '', message);
 } else if (hour === workStartTime && minute === 0 && second === 0) {
-  $notification.post('上班提醒', '', '现在是上班时间，请准备工作！');
+  const currentTime = now.format("hh:mm:ss");
+  $notification.post('上班提醒', '', `现在是北京时间 ${currentTime}\n请开始准备工作啦 😊`);
 } else if (hour === workEndTime && minute === 0 && second === 0) {
-  $notification.post('下班提醒', '', '现在是下班时间，辛苦一天，可以放松一下了！');
+  const currentTime = now.format("hh:mm:ss");
+  $notification.post('下班提醒', '', `现在是北京时间 ${currentTime}\n辛苦一天，可以放松一下了 😊`);
 } else {
-  $notification.post('工作时间提醒', '', '现在不是上班时间，请好好休息！');
+  const currentTime = now.format("hh:mm:ss");
+  $notification.post('工作时间提醒', '', `现在是北京时间 ${currentTime}\n放心玩耍吧 😊`);
 }
 
 $done();
+
+// Date 原型对象的 format 函数
+Date.prototype.format = function(fmt) {
+  var date = {
+    "M+": this.getMonth() + 1,
+    "d+": this.getDate(),
+    "h+": this.getHours(),
+    "m+": this.getMinutes(),
+    "s+": this.getSeconds(),
+    "q+": Math.floor((this.getMonth() + 3) / 3),
+    "S": this.getMilliseconds()
+  };
+  if (/(y+)/i.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+  }
+  for (var k in date) {
+    if (new RegExp("(" + k + ")").test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, RegExp.$1.length == 1 ? date[k] : ("00" + date[k]).substr(("" + date[k]).length));
+    }
+  }
+  return fmt;
+};
